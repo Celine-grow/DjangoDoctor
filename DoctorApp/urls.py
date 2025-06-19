1d223af97caec55ecab98cab109c4f5a1c83540a
Naomi Bukusi <naomibukusi@gmail.com>
2025-06-19 15:13:11 +0300

    fist ui iteration

--- DoctorApp/urls.py
+++ DoctorApp/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_doctor, name='register_doctor'),
-    path('login/', views.login_doctor, name='login_doctor'),
-    path('dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
-    path('logout/', views.logout_doctor, name='logout_doctor'),
+#     path('register/', views.register_doctor, name='register_doctor'),
+#     path('login/', views.login_doctor, name='login_doctor'),
+#     path('dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
+#     path('logout/', views.logout_doctor, name='logout_doctor'),
+# 
+ path('', views.login, name='login'),
+ path('dashboard/', views.dashboard, name='dashboard'),
+ path('patients/', views.patients, name='patients'),  
+ path('chat/', views.chat, name='chat'),
+ path('cystela/', views.cystela, name='cystela'),  
+ path('settings/', views.settings, name='settings'),  
+
 ]
